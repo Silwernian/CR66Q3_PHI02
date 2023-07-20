75572b6e1a00af4f6c8627bd4b1c3b165422b1f8
Silwernian <62892463+Silwernian@users.noreply.github.com>
2023-07-21 02:53:59 +0700

    Add files via upload

---
+++ pages/11_❌_11_Fluid_Mechanics_II_-_Dynamics.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import toml
+
+
+def load_config(config_path):
+    with open(config_path, 'r') as f:
+        config = toml.load(f)
+    return config
+
+config = load_config('config.toml')
+
+st.set_page_config(page_title='Lecture 11', page_icon='\U0001F412')
+
+st.header(':violet[**Lecture 11 ยังไม่ Update น้าาา . . . :sob:**]')
